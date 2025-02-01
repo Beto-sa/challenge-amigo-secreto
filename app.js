fbc2e77ffa9a13e402f4225cac6c6c0086775c72
Beto-sa <adalbertosilvaalmeida@gmail.com>
2025-02-01 11:17:11 -0300

    Adicionando nomes e limpando o campo.

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
+// Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaAmigos = []; //Array para armazenar os nomes.
+
+//Função para exibis a lista de amigos no HTML.
+function exibirAmigos(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+} 
+
+// Função para adicionar os nomes no Array.
+function adicionarAmigo() {
+    let amigos = document.querySelector('input').value;//Armazena o valor recebido no HTML na variável "amigos".
+    listaAmigos.push(amigos);//Joga o valor armazenado na variável "amigos" para dentro do Array.
+    console.log(listaAmigos);
+    
+    if (document.querySelector('input').value == "") {
+        alert('Por favor, insira um nome.');
+    } else {
+        let listaFinal = "";
+        for ( let i = 0; i < listaAmigos.length; i++ ) {
+            listaFinal += listaAmigos[i] 
+            listaFinal += i < listaAmigos.length - 1 ? ", " : "."; 
+        }
+        exibirAmigos('ul', listaFinal);
+        limparCampo();
+    }
+}
+
+//funcção para Limpar campo.
+function limparCampo() {
+    let retornoVazio = document.querySelector('input');
+    retornoVazio.value = '';
+}
